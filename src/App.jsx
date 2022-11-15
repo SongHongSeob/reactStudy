3e0176601a8b17cf951279e0045261ca5736bae2
SongHongSeob <51102840+SongHongSeob@users.noreply.github.com>
2022-11-15 09:56:06 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Box from "./Box"
+
+export default function App() {
+  return (
+    <main>
+      <Box name="코알누" num="1"/>
+      <Box name="영선" num="2"/>
+      <Box name="상우" num="3"/>
+      <Box name="성권" num="4"/>
+      <Box name="홍섭" num="5"/>
+    </main>
+  )
+}
