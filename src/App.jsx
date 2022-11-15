5744e6865fa1700d554bf953c4ca4a60a0fae510
SongHongSeob <51102840+SongHongSeob@users.noreply.github.com>
2022-11-15 12:28:32 +0000

    기본적인 React.js의 state 기본 공부

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import './App.css'
-import Box from "./Box"
+import './App.css';
+import React,{useState} from 'react'
 
 export default function App() {
+  let count = 0
+  const [count2,setCount2] = useState(0)
+  const increase = () =>{
+    count = count+1  // state를 update 하기 위해 함수 전체가 다시 진행되므로 0으로 초기화 된다.
+    setCount2(count2+1)
+    console.log("count work?",count,"state Count2",count2)
+  }
   return (
     <main>
-      <Box name="코알누" num="1"/>
-      <Box name="영선" num="2"/>
-      <Box name="상우" num="3"/>
-      <Box name="성권" num="4"/>
-      <Box name="홍섭" num="5"/>
+      <div>{count}</div>
+      <div>state:{count2}</div>
+      <button onClick={increase}>증가</button>
     </main>
   )
 }
+
+
+// state 변수값들은 비동기적이다. 함수가 전체가 끝나야 변수값이 한번에 변경된다.
